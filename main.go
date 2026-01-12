0d18624b2290165661c3d8dd6961046a38087687
Anshit-Token13 <84705984+ansh-token13@users.noreply.github.com>
2026-01-12 14:14:42 +0400

    Making Code Port 8800

--- main.go
+++ main.go
@@ -70,8 +70,8 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 	})
 
-	log.Println("Listening on :4444")
-	log.Fatal(http.ListenAndServe(":4444", nil))
+	log.Println("Listening on :8800")
+	log.Fatal(http.ListenAndServe(":8800", nil))
 }
 
 func initDatabase() {
